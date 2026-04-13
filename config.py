e585a520c362060a4bcebe01aacda9b86dd1feaf
nekiq1 <nekruzpro@mail.ru>
2026-04-13 13:12:22 +0300

    Add config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN") or os.getenv("BOT_TOKEN", "")
+
+ADMIN_ID = int(os.getenv("ADMIN_ID", "7642780807"))
+
+CHANNEL_ID = int(os.getenv("CHANNEL_ID", "-1003751381022"))
+CHANNEL_LINK = os.getenv("CHANNEL_LINK", "https://t.me/manikqure")
+
+SCHEDULE_CHANNEL_ID = int(os.getenv("SCHEDULE_CHANNEL_ID", str(CHANNEL_ID)))
+
+DB_PATH = os.getenv("DB_PATH", "database.sqlite3")
