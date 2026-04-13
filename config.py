d631960019cf72a1026585c0eb38bbcfb62acd83
nekiq1 <nekruzpro@mail.ru>
2026-04-13 14:04:10 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN") or os.getenv("TELEGRAM_BOT_TOKEN", "")
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", " ")
 
 ADMIN_ID = int(os.getenv("ADMIN_ID", "7642780807"))
 
